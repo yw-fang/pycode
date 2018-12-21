8f26354a23a81c47a95717ba7d0228017a615613
yw-fang <fyuewen@gmail.com>
2018-12-21 17:36:15 +0900

    setup.py created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycode',
+      version='0.1',
+      description='A repo for testing the source code magnagement',
+      url='https://github.com/yw-fang/pycode',
+      author='Yue-Wen FANG',
+      author_email='fyuewen@gmail.com',
+      license='MIT',
+      packages=['pycode'],
+      zip_safe=False)

01fa1db602c4355fbc8646cbb4387e5fd1d914ea
yw-fang <fyuewen@gmail.com>
2018-12-21 21:14:45 +0900

    add readme function to setup.py, add long_description and classifiers

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return(f.read())
+    
+
 setup(name='pycode',
       version='2.0',
       description='A repo for testing the source code magnagement',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: v2.0',
+          'License:: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Pthon package',
+          ],
+      keywords='python code package tutorial',
       url='https://github.com/yw-fang/pycode',
       author='Yue-Wen FANG',
       author_email='fyuewen@gmail.com',
@@ -11,4 +24,7 @@ setup(name='pycode',
       install_requires=[
          'markdown', 
           ],
+      include_package_data=True,
       zip_safe=False)
+
+

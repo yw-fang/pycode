8f1d74b0612a59e5595d5209859a50207a48601a
yw-fang <fyuewen@gmail.com>
2018-12-21 18:17:37 +0900

    upgrade to version 0.2, in this version, markdown module is required

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycode',
-      version='0.1',
+      version='0.2',
       description='A repo for testing the source code magnagement',
       url='https://github.com/yw-fang/pycode',
       author='Yue-Wen FANG',
